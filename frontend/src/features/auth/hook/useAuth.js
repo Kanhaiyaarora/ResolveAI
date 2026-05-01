062825deaabb55df593ca8f377839e63c50d7520
Kanhaiya Arora <kanhaiyaarora75@gmail.com>
2026-05-02 05:07:18 +0530

    feat(auth): implement authentication slice, API service, and custom hooks for user management

---
+++ frontend/src/features/auth/hook/useAuth.js
@@ -0,0 +1,81 @@
+import { setUser, setError, setLoading } from "../state/auth.slice";
+import { register, login, logout, getMe } from "../service/auth.api";
+import { useDispatch } from "react-redux";
+
+export const useAuth = () => {
+  const dispatch = useDispatch();
+
+  const handleRegisterUser = async ({
+    name,
+    email,
+    password,
+    role,
+    companyId,
+  }) => {
+    dispatch(setLoading(true));
+    dispatch(setError(null));
+    try {
+      const data = await register({
+        name,
+        email,
+        password,
+        role,
+        companyId,
+      });
+      dispatch(setUser(data.user));
+      return data.user;
+    } catch (error) {
+      dispatch(setError(error.response?.data?.message || error.message));
+      return null;
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+
+  const handleLoginUser = async ({ email, password }) => {
+    dispatch(setLoading(true));
+    dispatch(setError(null));
+    try {
+      const data = await login({ email, password });
+      dispatch(setUser(data.user));
+      return data.user;
+    } catch (error) {
+      dispatch(setError(error.response?.data?.message || error.message));
+      return null;
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+
+  const handleLogoutUser = async () => {
+    dispatch(setLoading(true));
+    dispatch(setError(null));
+    try {
+      await logout();
+      dispatch(setUser(null));
+    } catch (error) {
+      dispatch(setError(error.response?.data?.message || error.message));
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+
+  const handleGetMe = async () => {
+    try {
+      dispatch(setLoading(true));
+      const data = await getMe();
+      dispatch(setUser(data.user));
+    } catch (error) {
+      dispatch(setError(error.response?.data?.message || error.message));
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+
+  return {
+    handleRegisterUser,
+    handleLoginUser,
+    handleLogoutUser,
+    handleGetMe,
+  };
+};
